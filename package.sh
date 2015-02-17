cab7537126e612e1393c2e801149aefa05b16f3b
Justin Gilman <justin@cirle.com>
2015-02-17 15:27:56 -0500

    adding debian packaging script

---
+++ package.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+LIBVERSION=1.0.0
+LASTCOMMIT="Initial packaging commit"
+
+LIBPROJECTNAME=opencvblobs
+LIBNAME=lib$LIBPROJECTNAME
+TARBALLPATH=$LIBNAME-$LIBVERSION
+
+#build tarball
+mkdir -p $TARBALLPATH/src
+cp src/* $TARBALLPATH/src/
+cp $LIBPROJECTNAME.pro $TARBALLPATH/
+echo "VERSION = $LIBVERSION" >> $TARBALLPATH/src/src.pro
+tar cfJ $LIBNAME_$LIBVERSION.orig.tar.xz $TARBALLPATH
+
+cp -r debian $TARBALLPATH/
+
+export PATH=/opt/Qt/5.2.1/gcc_64/bin:$PATH
+cd $TARBALLPATH
+echo $(pwd)
+dch --create -v $LIBVERSION --package $LIBNAME "$LASTCOMMIT"
+dch --release "Automated Package Build"
+
+fakeroot debian/rules clean build install binary
